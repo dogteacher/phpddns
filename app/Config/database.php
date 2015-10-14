bec09e4720d5b55068f0402150c37294cf2cfb3e
root <root@dogteacher.com>
2015-10-15 08:45:17 +0900

    database config set

---
+++ app/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $phpddns = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'phpuser',
+		'password' => 'phpddns',
+		'database' => 'phpddns',
+		'encoding' => 'utf8'
+	);
+}
